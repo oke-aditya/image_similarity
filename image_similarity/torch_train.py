79914ead4aa23d8b2347fec66b9c4795c6c10f8a
Aditya Oke <okeaditya315@gmail.com>
2020-08-20 08:23:04 +0530

    Completed Training Code

--- image_similarity/torch_train.py
+++ image_similarity/torch_train.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     utils.seed_everything(config.SEED)
 
     transforms = T.Compose([T.ToTensor()])
+    print("------------ Creating Dataset ------------")
     full_dataset = torch_data.FolderDataset(config.IMG_PATH, transforms)
 
     train_size = int(config.TRAIN_RATIO * len(full_dataset))
@@ -25,12 +26,16 @@ if __name__ == "__main__":
         full_dataset, [train_size, val_size]
     )
 
+    print("------------ Dataset Created ------------")
+    print("------------ Creating DataLoader ------------")
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=config.TRAIN_BATCH_SIZE, shuffle=True, drop_last=True
     )
     val_loader = torch.utils.data.DataLoader(
         val_dataset, batch_size=config.TEST_BATCH_SIZE
     )
+
+    print("------------ Dataloader Cretead ------------")
 
     # print(train_loader)
     loss_fn = nn.MSELoss()
@@ -49,10 +54,24 @@ if __name__ == "__main__":
     autoencoder_params = list(encoder.parameters()) + list(decoder.parameters())
     optimizer = optim.AdamW(autoencoder_params, lr=config.LEARNING_RATE)
 
+    # early_stopper = utils.EarlyStopping(patience=5, verbose=True, path=)
+    max_loss = 9999
+
+    print("------------ Training started ------------")
+
     for epoch in tqdm(range(config.EPOCHS)):
         train_loss = torch_engine.train_step(
             encoder, decoder, train_loader, loss_fn, optimizer, device
         )
+        print(f"Epochs = {epoch}, Training Loss : {train_loss}")
         val_loss = torch_engine.val_step(encoder, decoder, val_loader, loss_fn, device)
+
+        # Simple Best Model saving
+        if val_loss < max_loss:
+            print("Validation Loss decreased, saving new best model")
+            torch.save(encoder.state_dict(), config.ENCODER_MODEL_PATH)
+            torch.save(decoder.state_dict(), config.DECODER_MODEL_PATH)
+
+        print(f"Epochs = {epoch}, Validation Loss : {val_loss}")
 
     print("Training Done")
